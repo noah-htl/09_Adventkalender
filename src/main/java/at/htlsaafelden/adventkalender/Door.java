adbd70e6aa31005d6539dbac4e49c9646dbb0f5e
Noah <82201000+Nopro263@users.noreply.github.com>
2024-12-10 14:47:22 +0100

    added popup

--- src/main/java/at/htlsaafelden/adventkalender/Door.java
+++ src/main/java/at/htlsaafelden/adventkalender/Door.java
@@ -1,15 +1,14 @@
 package at.htlsaafelden.adventkalender;
 
-import javafx.beans.property.adapter.JavaBeanIntegerProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Screen;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -54,6 +53,20 @@ public class Door extends AnchorPane {
 
     private void open() {
         this.borderPane.getStyleClass().add("open");
+
+        FXMLLoader fxmlLoader = new FXMLLoader(AdventApplication.class.getResource("wordle-view.fxml"));
+        Stage stage = new Stage();
+        Scene scene = null;
+        try {
+            scene = new Scene(fxmlLoader.load(), Screen.getPrimary().getBounds().getWidth() / 2,
+                    Screen.getPrimary().getBounds().getHeight() / 2);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        stage.setTitle("Wordle!");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public void setNumber(int x) {
